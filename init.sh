298e8eb4b3b3e85b3186655d68179c4d979afa23
Givi Tsvariani <thommaster10@gmail.com>
2023-07-07 20:45:58 -0400

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install flatpak gnome-software-plugin-flatpak snapd
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
