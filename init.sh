7ec893ffeb4411379b790c2b7b75a8b16d3b88e2
Givi Tsvariani <thommaster10@gmail.com>
2023-07-07 20:47:11 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install flatpak gnome-software-plugin-flatpak snapd
+sudo apt install flatpak gnome-software-plugin-flatpak snapd curl wget vim
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
