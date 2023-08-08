f0956e8e02d878e79f58dab365bb2898f42a376c
Givi Tsvariani <thommaster10@gmail.com>
2023-08-08 15:52:36 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+##### update packages
+
+sudo apt update -y
+sudo apt-get update -y
+sudo apt upgrade -y
+sudo apt-get upgrade -y
+
+##### install basic necessities
+
+sudo apt install git curl wget unzip
+
+##### setup package managers
+#### Flatpak
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+#### Snap
+sudo apt install snapd
+#### NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+#### NPM
+nvm install --lts
+
+echo "System must be rebooted"
+echo "run ./setup2.sh after the reboot"
+sleep 5
+reboot
